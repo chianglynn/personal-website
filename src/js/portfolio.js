d8686fe5fe84393850cc5b2fc58cda37afcda830
Chiang Lynn <lynnchiangtw@gmail.com>
2021-08-24 18:12:14 +0800

    Revise skills

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -5,13 +5,13 @@ export const portfolio = [
         github: 'https://github.com/chianglynn/frontendmentor-easybank-landing-page',
         demo: 'https://chianglynn.github.io/frontendmentor-easybank-landing-page/',
         image: 'https://github.com/chianglynn/frontendmentor-easybank-landing-page/raw/main/src/screenshot/screenshot.png?raw=true',
-        skills: ['JavaScript'],
+        skills: ['HTML', 'CSS', 'JavaScript'],
     },
     {
         title: 'REST Countries API with color theme switcher',
         github: 'https://github.com/chianglynn/frontendmentor-rest-countries-api-with-color-theme-switcher',
         demo: 'https://chianglynn.github.io/frontendmentor-rest-countries-api-with-color-theme-switcher/',
         image: 'https://github.com/chianglynn/frontendmentor-rest-countries-api-with-color-theme-switcher/raw/main/src/screenshot/screenshot-light.png?raw=true',
-        skills: ['JavaScript'],
+        skills: ['HTML', 'CSS', 'JavaScript'],
     },
 ];
